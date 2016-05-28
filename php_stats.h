4fb95253cce347e976a188a66953223b2e1053ac
Yuji Uchiyama <y.uchiyama.1015@gmail.com>
2016-05-28 14:04:49 +0900

    Update package information

--- php_stats.h
+++ php_stats.h
@@ -24,7 +24,7 @@
 extern zend_module_entry stats_module_entry;
 #define phpext_stats_ptr &stats_module_entry
 
-#define PHP_STATS_VERSION "1.0.3-dev"
+#define PHP_STATS_VERSION "1.0.4-dev"
 
 #ifdef PHP_WIN32
 #define PHP_STATS_API __declspec(dllexport)
